b2d79f56fb17b8b42f1356f5dcba272ecdf17872
Vignesh Muralidharan <vignesh.em@swiggy.in>
2022-11-12 13:01:55 +0530

    Add CustomCurveView

---
+++ app/src/main/java/com/example/curvestructures/view/CustomCurveView.kt
@@ -0,0 +1,13 @@
+package com.example.curvestructures.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class CustomCurveView @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0,
+) : View(context, attributeSet, defStyleAttr, defStyleRes) {
+}
